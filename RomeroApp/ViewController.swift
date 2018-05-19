690b5a53328d816cb141a3303e62d5648c36fd07
oltv00 <oltv00@gmail.com>
2018-05-19 21:39:10 +0300

    Project init

---
+++ RomeroApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RomeroApp
+//
+//  Created by Олег В. Твердохлеб on 19/05/2018.
+//  Copyright © 2018 San Jose Romero App iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
